b486c27e509568e8940921eec56449cb1c5ad2c8
RichHong <rich.hong@gmail.com>
2016-11-11 20:45:40 -0500

    fixed broken images

--- public/src/components/focus.js
+++ public/src/components/focus.js
@@ -8,6 +8,11 @@ class Focus extends Component {
     const focus = document.getElementById('focus');
     focus.scrollIntoView({block:'end', behavior:'smooth'});
   }
+  _imgError(image) {
+    image.onerror = '';
+    image.src = 'http://vignette3.wikia.nocookie.net/logopedia/images/e/ec/MLB.png';
+    return true;
+  }
   render () {
     let {away_team_city, away_team_name, home_team_city, home_team_name, venue, location} = this.props.game;
     let pic; 
@@ -19,7 +24,7 @@ class Focus extends Component {
           <br/>
           <span>{`${home_team_city} ${home_team_name}`}</span>
         </div>
-        <img className='focus-thumbnail thumbnail' src={pic} />
+        <img className='focus-thumbnail thumbnail' src={pic} onError={err => this._imgError(this.img)} ref={img => this.img = img}/>
         <div id='bottom' className='bottom-box box'>
           <span>{`${venue}`}</span>
           <br/>
--- public/src/components/game.js
+++ public/src/components/game.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class Game extends Component {
+  _imgError(image) {
+    image.onerror = '';
+    image.src = 'http://vignette3.wikia.nocookie.net/logopedia/images/e/ec/MLB.png';
+    return true;
+  }
   render(){
     let pic; 
     this.props.game.video_thumbnails ? pic = this.props.game.video_thumbnails.thumbnail[0].content : pic = 'http://vignette3.wikia.nocookie.net/logopedia/images/e/ec/MLB.png';
     return(
       <div className='game'>
-        <img className='thumbnail' src={pic} />
+        <img className='thumbnail' src={pic} onError={err => this._imgError(this.img)} ref={img => this.img = img} />
       </div>
     );
   }
