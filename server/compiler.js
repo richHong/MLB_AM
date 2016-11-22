727b576d885dc77acbdd454f06c790f48a2ab191
Richard Hong <rich.hong@gmail.com>
2016-11-21 20:23:10 -0800

    Update compiler.js
    made more readable

--- server/compiler.js
+++ server/compiler.js
@@ -2,16 +2,20 @@
 const Webpack          = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const WebpackConfig    = require('./../webpack.config.js');
+
 module.exports = () => {
   let bundleStart = null;
   const compiler = Webpack(WebpackConfig);
+  
   compiler.plugin('compile', () => {
     console.log('Bundling...');
     bundleStart = Date.now();
   });
+  
   compiler.plugin('done', () => {
     console.log('Bundled in ' + (Date.now() - bundleStart) + 'ms!');
   });
+  
   const bundler = new WebpackDevServer(compiler, {
     publicPath: '/build/',
     hot: true,
@@ -21,5 +25,6 @@ module.exports = () => {
       colors: true
     }
   });
+  
   bundler.listen(8080, () => console.log('Bundling project, please wait...'));
 };
